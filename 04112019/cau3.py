8da574e5bd2889e97424fe3e3c8b6713e2b7a291
dachanh <ghostcodert&@gmail.com>
2019-11-05 09:14:02 +0700

    convert RGB to BGR

--- 04112019/cau3.py
+++ 04112019/cau3.py
@@ -5,12 +5,13 @@ data = open('data.txt','r').readlines()
 v  , b= [[],[],[]] , []
 for indx ,line in enumerate(data):
     temp = line.split()
-    v[0].append(int(temp[0]))
-    v[1].append(int(temp[1]))
-    v[2].append(int(temp[2]))
+    v[2].append(int(temp[0])) 
+    v[1].append(int(temp[1])) 
+    v[0].append(int(temp[2])) 
     b.append(int(temp[3]))
  
 img = cv2.imread('./lion.jpg')
+k = cv2.copyMakeBorder(img,0,0,0,0,cv2.BORDER_REPLICATE) 
 m,n = img.shape[:2]
 ID = []
 for idx,it in enumerate(b):
@@ -19,10 +20,10 @@ for idx,it in enumerate(b):
 for c in range(3):
     for it in ID: v[c][it] = 0
     for j in range(n):
-        count = 0
+        count = 1
         for i in range((j-1)*m + 1 ,j*m):
-            if v[c][i] == 0 :
-                img[count][j][c] = 0 
+            k[count,j,c] = v[c][i]
             count += 1
-cv2.imshow('k',img)
+k =np.array(k,np.uint8)
+cv2.imshow('k',k)
 cv2.waitKey(0)
